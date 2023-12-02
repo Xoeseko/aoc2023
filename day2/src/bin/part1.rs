13a3f2b4d582b3153f494c0af8dfe45537bf77fa
Xoeseko Nyomi <xoeseko@gmail.com>
2023-12-02 10:16:35 +0100

    chore: Initial day2 folder

---
+++ day2/src/bin/part1.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("input.txt");
+    dbg!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(1, 1);
+    }
+}
